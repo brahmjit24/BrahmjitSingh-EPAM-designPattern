4c3a95d5dde7b3028a778b9492134158cf1f507f
brahmjit24 <47661205+brahmjit24@users.noreply.github.com>
2020-03-10 23:11:25 +0530

    work done

---
+++ src/main/java/Brahmjit/designPattern/structural/IteratorPattern.java
@@ -0,0 +1,45 @@
+package Brahmjit.designPattern.structural;
+
+/**
+ * author @ Brahmjit-Singh
+ *
+ */
+import java.util.Iterator;
+abstract class OuterCompanyContainer {
+    public abstract Iterator<String> getIterator();
+}
+
+class NamesDeck extends OuterCompanyContainer {
+
+    public String namesArr[]={"A1", "A2","A3"};
+    public Iterator<String> getIterator() {
+        return new NamesIterator() ;
+    }
+    private class NamesIterator implements Iterator<String> {
+        int i;
+        public boolean hasNext() {
+            if (i<namesArr.length){
+                return true;
+            }
+            return false;
+        }
+        
+        public String next() {
+            if(this.hasNext()){
+                return namesArr[i++];
+            }
+            return null;
+        }
+    }
+}
+
+class IteratorPattern  {
+    public static void main(String[] args) {
+        NamesDeck myCollection = new NamesDeck();
+        Iterator<String> itr = myCollection.getIterator();
+        while(itr.hasNext()){
+            String name = (String)itr.next();
+            System.out.println("Name : " + name);
+        }
+    }
+}
